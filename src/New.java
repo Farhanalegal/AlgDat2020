5338b9d85a4a6874557817ec2379748647711c8a
Farhan <s341869@oslomet.no>
2020-08-24 15:30:35 +0200

    Initial commit

---
+++ src/New.java
@@ -0,0 +1,13 @@
+public class New {
+    public static void main(String[] args) {
+        System.out.println("Hallo World");
+        System.out.println("Teller til ti");
+        for (int i = 1; i <= 10; ++i) {
+            System.out.println(i);
+        }
+        System.out.println("Peter teller til 15");
+        for (int i = 11; i <= 15; ++i) {
+            System.out.println("Peter teller" + i);
+        }
+    }
+}
