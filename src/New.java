ee32b18c992b7da959dbdc2d4efa03aaeab09437
Farhan <s341869@oslomet.no>
2020-08-24 16:30:59 +0200

    Peter teller til 20.

--- src/New.java
+++ src/New.java
@@ -5,9 +5,9 @@ public class New {
         for (int i = 1; i <= 10; ++i) {
             System.out.println(i);
         }
-        System.out.println("Peter teller til femten");
-        for (int i = 11; i <15;++i){
-            System.out.println("Peter teller og sier" + i);
-        }
+        System.out.println("Peter teller til tyve");
+        for (int i = 11; i <=20;++i){
+                System.out.println("Peter teller og sier" + i);
     }
+}
 }
