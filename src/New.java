af40c056d76a663230eb6d45bfe9292c5367e48e
Farhan <s341869@oslomet.no>
2020-08-24 16:15:49 +0200

    Peter teller til 50.

--- src/New.java
+++ src/New.java
@@ -5,9 +5,9 @@ public class New {
         for (int i = 1; i <= 10; ++i) {
             System.out.println(i);
         }
-        System.out.println("Peter teller til 50");
+        System.out.println("Andre teller til femti");
         for (int i = 11; i <50;++i){
-            System.out.println("Peterer og sier " + i);
+            System.out.println("Andre teller og sier" + i);
         }
     }
 }
