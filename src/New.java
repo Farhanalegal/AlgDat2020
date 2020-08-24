873f33f75f0b26c266b186b4f8f520332f4331cd
Farhan <s341869@oslomet.no>
2020-08-24 16:35:14 +0200

    Andra teller og sier til femti

--- src/New.java
+++ src/New.java
@@ -5,9 +5,9 @@ public class New {
         for (int i = 1; i <= 10; ++i) {
             System.out.println(i);
         }
-        System.out.println("Peter teller til tyve");
-        for (int i = 11; i <=20;++i){
-                System.out.println("Peter teller og sier" + i);
+        System.out.println("Andre teller til femti");
+        for (int i = 11; i <50;++i){
+                System.out.println("Andra teller og sier " + i);
     }
 }
 }
